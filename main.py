af2ae2ac37a5f7e489f62c2d53d5fed819d92ef2
taadams1 <rainedoescode@gmail.com>
2023-07-17 10:06:45 -0500

    Further color average testing, beginning of display refactor

--- main.py
+++ main.py
@@ -10,13 +10,18 @@ import PIL
 from sklearn.cluster import KMeans
 
 
-def display_img_comparison(img_1, img_2):
+def display_img_comparison(img_1, img_2, img_3, img_4):
     # set up format and axis
-    fmt, ax = plt.subplots(1, 2, figsize=(10, 10))
-    ax[0].imshow(img_1)
-    ax[1].imshow(img_2)
-    ax[0].axis('off')  # hide axis on display
-    ax[1].axis('off')
+    fmt, ax = plt.subplots(2, 2, figsize=(10, 10))
+    ax[0, 0].imshow(img_1)
+    ax[0, 1].imshow(img_2)
+    ax[1, 0].imshow(img_3)
+    ax[1, 1].imshow(img_4)
+    ax[0, 0].axis('off')  # hide axis on display
+    ax[0, 1].axis('off')
+    ax[1, 0].axis('off')
+    ax[1, 1].axis('off')
+
     fmt.tight_layout()
     plt.show()
 
@@ -25,13 +30,19 @@ img = cv2.imread("block/nether_quartz_ore.png")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_2 = cv2.imread("block/stripped_cherry_log.png")
 img_2 = cv2.cvtColor(img_2, cv2.COLOR_BGR2RGB)
+img_3 = cv2.imread("block/exposed_cut_copper.png")
+img_3 = cv2.cvtColor(img_3, cv2.COLOR_BGR2RGB)
+img_4 = cv2.imread("block/moss_block.png")
+img_4 = cv2.cvtColor(img_4, cv2.COLOR_BGR2RGB)
 
-dim = (320, 320)
+dim = (512, 512)
 # resize image with nearest neighbor
-img = cv2.resize(img, dim, interpolation=cv2.INTER_NEAREST)
-img_2 = cv2.resize(img_2, dim, interpolation=cv2.INTER_NEAREST)
+img1_big = cv2.resize(img, dim, interpolation=cv2.INTER_NEAREST)
+img2_big = cv2.resize(img_2, dim, interpolation=cv2.INTER_NEAREST)
+img3_big = cv2.resize(img_3, dim, interpolation=cv2.INTER_NEAREST)
+img4_big = cv2.resize(img_4, dim, interpolation=cv2.INTER_NEAREST)
 
-display_img_comparison(img, img_2)
+display_img_comparison(img1_big, img2_big, img3_big, img4_big)
 
 
 # method 1 - average color
@@ -116,3 +127,20 @@ display_img_comparison(img, palette_percent(cluster_1))
 
 cluster_2 = clt.fit(img_2.reshape(-1, 3))
 display_img_comparison(img_2, palette_percent(cluster_2))
+
+
+# 4 - average of color percent cluster
+# This produces nearly identical results to whole image average
+
+cluster_1 = clt.fit(img.reshape(-1, 3))
+temp_img = palette_percent(cluster_1)
+temp_img[:, :, 0], temp_img[:, :, 1], temp_img[:,
+                                               :, 2] = np.average(img, axis=(0, 1))
+display_img_comparison(img, temp_img)
+# create image with average color of img 1
+
+cluster_2 = clt.fit(img_2.reshape(-1, 3))
+temp_img_2 = palette_percent(cluster_2)
+temp_img_2[:, :, 0], temp_img_2[:, :, 1], temp_img_2[:,
+                                                     :, 2] = np.average(img_2, axis=(0, 1))
+display_img_comparison(img_2, temp_img_2)
